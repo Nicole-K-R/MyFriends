dda733c58a28a34ae99e2d379d03c0c626bb2686
Nicole Rosario <nkrosario@rogers.com>
2018-01-28 04:44:07 -0500

    Updated all 3 people's get events

--- functions/intents/GetEvents.js
+++ functions/intents/GetEvents.js
@@ -2,6 +2,25 @@ const lib = require('lib');
 var jsonfile = require('jsonfile')
 var file = 'db.json'
 
+var getEvents = function(name, eventsLists){
+  // Create message for Nicole
+  var message = '';
+  for (var i = 0; i < eventsLists[name].length; i ++){
+    console.log(eventsLists[name][i][0]);
+    message += 'going to ' + eventsLists[name][i][0];
+    if (eventsLists[name][i][1] !== null){
+      console.log(eventsLists[name][i][1]);
+      message += ' on ' + eventsLists[name][i][1];
+    }
+    if (eventsLists[name][i][2] !== null){
+      console.log(eventsLists[name][i][2]);
+      message += ' at ' + eventsLists[name][i][2];
+    }
+    if (i !== eventsLists[name].length - 1) { message += ' and '; } 
+  }
+  return message;
+}
+
 // Log: lib logs Nicole.MyFriends[@dev].*
 /**
 * GetEvents intent, can receive a 'event', 'Date', and 'Time' parameter
@@ -35,22 +54,8 @@ module.exports = (Event = null, Date = null, Time = null, Name = null, callback)
   }
   console.log(eventsLists);
 
-  if (Name === 'Nicole'){
-    // Create message for Nicole
-    var message = '';
-    for (var i = 0; i < eventsLists['Nicole'].length; i ++){
-      console.log(eventsLists['Nicole'][i][0]);
-      message += 'going to ' + eventsLists['Nicole'][i][0];
-      if (eventsLists['Nicole'][i][1] !== null){
-        console.log(eventsLists['Nicole'][i][1]);
-        message += ' on ' + eventsLists['Nicole'][i][1];
-      }
-      if (eventsLists['Nicole'][i][2] !== null){
-        console.log(eventsLists['Nicole'][i][2]);
-        message += ' at ' + eventsLists['Nicole'][i][2];
-      }
-      if (i !== eventsLists['Nicole'].length - 1) { message += ' and '; } 
-    }
+  if (Name === 'Nicole'){ // Nicole
+    var message = getEvents(Name, eventsLists);
     console.log(message);
     return callback(null, {
       outputSpeech: {
@@ -60,79 +65,46 @@ module.exports = (Event = null, Date = null, Time = null, Name = null, callback)
       card: {
         type: "Simple",
         title: "Get Events",
-        content: "Successfully got your events Nicole \n " + message
+        content: "Successfully got your events for Nicole \n Nicole is " +  message
       },
       shouldEndSession: true
     });
-  } else if (Name === 'Tracy'){
-    // Create message for Tracy
-    var message;
-    for (var i = 0; i < eventsLists['Tracy'].length; i ++){
-      if (Date === null && Time === null){
-        message += eventsLists['Tracy'][i][0];
-      }
-      if (Date !== null){
-        message += ' on ' + eventsLists['Tracy'][i][1];
-      }
-      if (Time !== null){
-        message += ' at ' + eventsLists['Tracy'][i][2];
-      }
-      if (i !== eventsLists['Tracy'].length - 1) { message += ' and '; } 
-    }
+  } 
+  else if (Name === 'Tracy'){ // Tracy
+    var message = getEvents(Name, eventsLists);
+    console.log(message);
     return callback(null, {
       outputSpeech: {
         type: 'PlainText',
-        text: err ? `Error: ${err.message}` : message
+        text: err ? `Error: ${err.message}` : 'Tracy is ' + message
       },
       card: {
         type: "Simple",
         title: "Get Events",
-        content: "Successfully got your events for Tracy\n " + message
+        content: "Successfully got your events for Tracy \n Tracy is " +  message
       },
       shouldEndSession: true
     });
   } else if (Name === 'Matt'){
-    // Create message for Matt
-    var message;
-    for (var i = 0; i < eventsLists['Matt'].length; i ++){
-      if (Date === null && Time === null){
-        message += eventsLists['Matt'][i][0];
-      }
-      if (Date !== null){
-        message += ' on ' + eventsLists['Matt'][i][1];
-      }
-      if (Time !== null){
-        message += ' at ' + eventsLists['Matt'][i][2];
-      }
-      if (i !== eventsLists['Matt'].length - 1) { message += ' and '; } 
-    }
+    var message = getEvents(Name, eventsLists);
+    console.log(message);
     return callback(null, {
       outputSpeech: {
         type: 'PlainText',
-        text: err ? `Error: ${err.message}` : message
+        text: err ? `Error: ${err.message}` : 'Matt is ' + message
       },
       card: {
         type: "Simple",
         title: "Get Events",
-        content: "Successfully got your events for Matt \n " + message
+        content: "Successfully got your events for Matt \n Matt is " +  message
       },
       shouldEndSession: true
     });
   } else if (Name === 'my friends'){
-    // Create message for my friends
-    var message;
-    // for (var i = 0; i < eventsLists['my friends'].length; i ++){
-    //   if (Date === null && Time === null){
-    //     message += eventsLists['my friends'][i].eventName;
-    //   }
-    //   if (Date !== null){
-    //     message += ' on ' + eventsLists['my friends'][i].eventDate;
-    //   }
-    //   if (Time !== null){
-    //     message += ' at ' + eventsLists['my friends'][i].eventTime;
-    //   }
-    //   if (i !== eventsLists['my friends'].length - 1) { message += ' and '; } 
-    // }
+    var message = 'Nicole is ' + getEvents('Nicole', eventsLists);
+    message += ' and Tracy is ' + getEvents('Tracy', eventsLists);
+    message += ' and Matt is ' + getEvents('Matt', eventsLists);
+    console.log(message);
     return callback(null, {
       outputSpeech: {
         type: 'PlainText',
